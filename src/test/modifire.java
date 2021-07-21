0ac74841cbe722780fb27874bc983c173bb898e0
NikitaKoba <49489487+NikitaKoba@users.noreply.github.com>
2021-07-21 22:06:32 +0300

    Generate Signature

--- src/test/modifire.java
+++ src/test/modifire.java
@@ -10,19 +10,19 @@ import java.util.Base64;
 
 
 public class modifire {
-    public String getSignature(String key, String request) throws Exception {
-        byte[] secretKey = key.getBytes("UTF8");
-        String algorithm = "HmacSHA1";
-        Mac mac = Mac.getInstance(algorithm);
-        mac.init(new SecretKeySpec(secretKey, algorithm));
-        byte[] hash = mac.doFinal(request.getBytes(StandardCharsets.UTF_8));
-        return Base64.getEncoder().encodeToString(hash);
+    public String getSignature(String key, String request) throws Exception {//Объявляем get Signature(Со стринговыми значениями key,requst)
+        byte[] secretKey = key.getBytes("UTF8"); //Объявление secretKey = получаение байтов для key.
+        String algorithm = "HmacSHA1"; //Объявляем алгоритм шифровки в стринг и передаем в getInstance
+        Mac mac = Mac.getInstance(algorithm);//Получаем из String algorithm значение "HmacSHA1"
+        mac.init(new SecretKeySpec(secretKey, algorithm)); //Инициализируем Mac передавая изначальный секретный ключ
+        byte[] hash = mac.doFinal(request.getBytes(StandardCharsets.UTF_8));//Кодируем реквест в ютф8,Метод doFinal () класса Mac используется для завершения операции Mac
+        return Base64.getEncoder().encodeToString(hash);//переводим в base64 в строку hash.
 
     }
 }
 class call {
     public static void main(String[] args) throws Exception {
-        modifire mynewGenerate = new modifire();
+        modifire mynewGenerate = new modifire(); //Объект класса
         System.out.println(mynewGenerate.getSignature("HN0ANDN7Mctg2G16","/api/v3/clients/2721196031/auth{\"dateTime\":\"2021-07-20T08:06:50\",\"dboType\":\"OMP\",\"dboClientId\":\"965\",\"login\":\"#2721196031\",\"device\":{\"uid\":\"ee01bcb5688f443c\",\"name\":\"SM-A105F\",\"system\":\"29\",\"type\":\"ANDROID\",\"vendor\":\"samsung\",\"ip\":\"87.238.235.149\",\"isNewDevice\":false},\"appVersion\":\"5.19.0\",\"authorizationType\":\"PASSWORD_LOGINASS\",\"remoteSoftwareTypes\":[],\"deviceHackFlg\":false}"));
 
     }
