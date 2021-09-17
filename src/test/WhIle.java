c8637e2aadf88c630a8750147f0d1df8360461d1
KobaNikita <Mactavish1811@gmail.com>
2021-09-17 20:48:59 +0300

    Modified WhIle.java

--- src/test/WhIle.java
+++ src/test/WhIle.java
@@ -3,9 +3,14 @@ import java.util.Scanner;
 public class WhIle {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
-        int min = 0;
-        while (console.hasNextInt()){
-            
+        int min = Integer.MAX_VALUE;//Присвоить МИН значение
+        while (console.hasNextInt()){// Пока вводятся целые числы
+            int x = console.nextInt();//Собираем ввод в x
+            if (x < min) {//Если x < min
+                min = x;// min = x
+            }
+            }
+        System.out.println(min);
         }
     }
-}
+
