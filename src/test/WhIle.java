4a748ef232f290574f2be257af2cfd267724f35c
KobaNikita <Mactavish1811@gmail.com>
2021-09-17 19:39:30 +0300

    add file WhIle.java to test branch

---
+++ src/test/WhIle.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class WhIle {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int min = 0;
+        while (console.hasNextInt()){
+            
+        }
+    }
+}
